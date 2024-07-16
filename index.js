f1640771784fb4086fdc1801f18a071675172515
Fousiya T <2388912@tcs.com>
2024-07-16 21:37:49 +0530

    changed

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express');  
-const fetch = require('node-fetch');  
-const app = express();  
-const port = 3000;  
-  
-app.set('view engine', 'ejs');  
-  
-app.get('/', (req, res) => {  
-  res.render('index');  
-});  
-  
-app.get('/chat', async (req, res) => {  
-  const message = req.query.message;  
-  const response = await fetch(`testbackenedapp.azurewebsites.net/chat?message=${message}`);  
-  const data = await response.text();  
-  res.send(data);  
-});  
-  
-app.listen(port, () => {  
-  console.log(`Frontend listening at http://localhost:${port}`);  
-});  
+const express = require('express');    
+const fetch = require('node-fetch');    
+const app = express();    
+const port = process.env.PORT || 3000;  // Use the PORT environment variable if it exists  
+    
+app.set('view engine', 'ejs');    
+    
+app.get('/', (req, res) => {    
+  res.render('index');    
+});    
+    
+app.get('/chat', async (req, res) => {    
+  const message = req.query.message;    
+  const response = await fetch(`testbackenedapp.azurewebsites.net/chat?message=${message}`);    
+  const data = await response.text();    
+  res.send(data);    
+});    
+    
+app.listen(port, () => {    
+  console.log(`Frontend listening at http://localhost:${port}`);    
+});    
